ffd08dd777c57de67ed3d047893a1b28ae12db03
WzDTj <j.dantong@gmail.com>
2021-10-03 15:06:04 +0800

    solved problem 724 & problem 1991

---
+++ problems/724.find-pivot-index.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int pivotIndex(vector<int> &nums) {
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+
+    int lsum = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (sum == lsum * 2 + nums[i]) {
+        return i;
+      }
+      lsum += nums[i];
+    }
+
+    return -1;
+  }
+};
