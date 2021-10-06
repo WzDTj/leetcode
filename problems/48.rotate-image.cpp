0b33e98b409366da5fb0174848630dfe5d879bdd
WzDTj <j.dantong@gmail.com>
2021-10-06 23:37:35 +0800

    solved problem 48

---
+++ problems/48.rotate-image.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  void rotate(vector<vector<int>> &matrix) {
+    int n = matrix.size();
+
+    // 左右翻转
+    for (int row = 0; row < n; ++row) {
+      for (int col = 0; col < (n >> 1); ++col) {
+        swap(matrix[row][col], matrix[row][n - 1 - col]);
+      }
+    }
+
+    // 斜对角翻转
+    for (int row = 0; row < n; ++row) {
+      for (int col = 0; col < n - row; ++col) {
+        swap(matrix[row][col], matrix[n - 1 - col][n - 1 - row]);
+      }
+    }
+  }
+};
