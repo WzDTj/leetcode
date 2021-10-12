7ec2e88baa1f83288ec83e0e1428a44ef07dda4e
WzDTj <j.dantong@gmail.com>
2021-10-12 09:49:12 +0800

    solved problem 28

---
+++ problems/28.implement-strstr.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  int strStr(string haystack, string needle) {
+    int haystack_length = haystack.length();
+    int needle_length = needle.length();
+
+    if (!needle_length)
+      return 0;
+
+    for (int i = 0; i < haystack_length; i++) {
+      if (haystack_length - i < needle_length)
+        break;
+
+      bool match = true;
+
+      for (int j = 0; j < needle_length; j++) {
+        if (haystack[i + j] != needle[j]) {
+          match = false;
+          break;
+        }
+      }
+
+      if (match)
+        return i;
+    }
+
+    return -1;
+  }
+};
