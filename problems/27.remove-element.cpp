098d7e9a4fe5114c3f0c52c7dcd0fea23f6f6c19
WzDTj <j.dantong@gmail.com>
2021-10-15 11:04:19 +0800

    added a solution for problem 27

--- problems/27.remove-element.cpp
+++ problems/27.remove-element.cpp
@@ -4,18 +4,34 @@ using namespace std;
 
 class Solution {
 public:
+  // 双指针优化
+
   int removeElement(vector<int> &nums, int val) {
     int n = nums.size();
 
-    int f = 0, s = 0;
+    int l = 0, r = n - 1;
 
-    while (f < n) {
-      if (nums[f] != val)
-        swap(nums[f], nums[s++]);
-
-      f++;
+    while (l <= r) {
+      nums[l] == val ? nums[l] = nums[r--] : l++;
     }
 
-    return s;
+    return l;
   }
+
+  // 快慢指针
+
+  // int removeElement(vector<int> &nums, int val) {
+  //   int n = nums.size();
+  //
+  //   int f = 0, s = 0;
+  //
+  //   while (f < n) {
+  //     if (nums[f] != val)
+  //       swap(nums[f], nums[s++]);
+  //
+  //     f++;
+  //   }
+  //
+  //   return s;
+  // }
 };
