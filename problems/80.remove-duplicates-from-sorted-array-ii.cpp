8c384672443890696fb9505dac40e732e8e68f2b
WzDTj <j.dantong@gmail.com>
2021-10-19 22:39:31 +0800

    added a solution for problem 80

--- problems/80.remove-duplicates-from-sorted-array-ii.cpp
+++ problems/80.remove-duplicates-from-sorted-array-ii.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 class Solution {
 public:
+  // 有序数组，保留 k 位
+
   int removeDuplicates(vector<int> &nums) {
     int n = nums.size();
     if (n < 2)
       return n;
 
-    int l = 1, r = 1, count = 1;
+    int l = 2, r = 2;
 
     while (r < n) {
-      nums[l - 1] == nums[r] ? count++ : count = 1;
-
-      if (nums[l - 1] <= nums[r] && count <= 2)
+      if (nums[l - 2] != nums[r])
         nums[l++] = nums[r];
 
       r++;
@@ -22,4 +22,25 @@ public:
 
     return l;
   }
+
+  // 利用 count 计数
+  //
+  // int removeDuplicates(vector<int> &nums) {
+  //   int n = nums.size();
+  //   if (n < 2)
+  //     return n;
+  //
+  //   int l = 1, r = 1, count = 1;
+  //
+  //   while (r < n) {
+  //     nums[l - 1] == nums[r] ? count++ : count = 1;
+  //
+  //     if (nums[l - 1] <= nums[r] && count <= 2)
+  //       nums[l++] = nums[r];
+  //
+  //     r++;
+  //   }
+  //
+  //   return l;
+  // }
 };
