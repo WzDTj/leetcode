1998b31317ce9d35f4fda2bec771913f10d0198c
WzDTj <j.dantong@gmail.com>
2021-10-21 20:43:38 +0800

    solved problem 217

---
+++ problems/217.contains-duplicate.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  bool containsDuplicate(vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+
+    for (int i = 1; i < nums.size(); ++i)
+      if (nums[i - 1] == nums[i])
+        return true;
+
+    return false;
+  }
+};
