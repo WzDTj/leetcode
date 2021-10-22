a6123504ea956538b8e58533ee7e2bd635bd4aff
WzDTj <j.dantong@gmail.com>
2021-10-22 09:22:26 +0800

    added a solution for problem 1

--- problems/1.two-sum.cpp
+++ problems/1.two-sum.cpp
@@ -1,40 +1,57 @@
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-  // 排序双指针
-
+  // 哈希表
   vector<int> twoSum(vector<int> &nums, int target) {
     int n = nums.size();
 
-    vector<int> answer(2);
-    vector<int> sorted_nums = vector<int>(nums);
-    sort(sorted_nums.begin(), sorted_nums.end());
+    unordered_map<int, int> map = unordered_map<int, int>();
 
-    int l = 0, r = n - 1;
-    while (l < r) {
-      int sum = sorted_nums[l] + sorted_nums[r];
-      if (sum < target)
-        ++l;
-      else if (target < sum)
-        --r;
-      else
-        break;
+    for (int i = 0; i < n; ++i) {
+      unordered_map<int, int>::iterator it = map.find(target - nums[i]);
+      if (it != map.end())
+        return {it->second, i};
+
+      map.insert({nums[i], i});
     }
-
-    for (int i = 0, j = 0; i < n; ++i) {
-      if (nums[i] != sorted_nums[l] && nums[i] != sorted_nums[r])
-        continue;
-
-      answer[j++] = i;
-
-      if (j == 2)
-        return answer;
-    }
-
     return {};
   }
+
+  // 排序双指针
+
+  // vector<int> twoSum(vector<int> &nums, int target) {
+  //   int n = nums.size();
+  //
+  //   vector<int> answer(2);
+  //   vector<int> sorted_nums = vector<int>(nums);
+  //   sort(sorted_nums.begin(), sorted_nums.end());
+  //
+  //   int l = 0, r = n - 1;
+  //   while (l < r) {
+  //     int sum = sorted_nums[l] + sorted_nums[r];
+  //     if (sum < target)
+  //       ++l;
+  //     else if (target < sum)
+  //       --r;
+  //     else
+  //       break;
+  //   }
+  //
+  //   for (int i = 0, j = 0; i < n; ++i) {
+  //     if (nums[i] != sorted_nums[l] && nums[i] != sorted_nums[r])
+  //       continue;
+  //
+  //     answer[j++] = i;
+  //
+  //     if (j == 2)
+  //       return answer;
+  //   }
+  //
+  //   return {};
+  // }
 };
