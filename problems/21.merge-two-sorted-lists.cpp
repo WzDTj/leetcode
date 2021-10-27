043be088573a11c2a19598dbf468bcca4157d857
WzDTj <j.dantong@gmail.com>
2021-10-27 09:03:24 +0800

    solved problem 21

---
+++ problems/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,29 @@
+#include "../structures/list_node.cpp"
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    ListNode *head = new ListNode(-1);
+    ListNode *pre = head;
+
+    while (l1 != nullptr && l2 != nullptr) {
+      if (l1->val < l2->val) {
+        pre->next = l1;
+        l1 = l1->next;
+      } else {
+        pre->next = l2;
+        l2 = l2->next;
+      }
+
+      pre = pre->next;
+    }
+
+    pre->next = l1 != nullptr ? l1 : l2;
+
+    return head->next;
+  }
+};
