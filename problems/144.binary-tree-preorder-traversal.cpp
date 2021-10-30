f3ddbdddc6876f5dc12ded551bc4fe1201684e2f
WzDTj <j.dantong@gmail.com>
2021-10-30 10:41:22 +0800

    added a solution for problem 144

--- problems/144.binary-tree-preorder-traversal.cpp
+++ problems/144.binary-tree-preorder-traversal.cpp
@@ -6,24 +6,43 @@ using namespace std;
 
 class Solution {
 public:
+  // 递归
+
   vector<int> preorderTraversal(TreeNode *root) {
-    if (root == nullptr)
-      return {};
-
     vector<int> answer;
-    stack<TreeNode *> stack({root});
-
-    while (!stack.empty()) {
-      TreeNode *front = stack.top();
-      stack.pop();
-
-      answer.push_back(front->val);
-      if (front->right != nullptr)
-        stack.push(front->right);
-      if (front->left != nullptr)
-        stack.push(front->left);
-    }
-
+    traversal(answer, root);
     return answer;
+  }
+
+  // 迭代
+
+  // vector<int> preorderTraversal(TreeNode *root) {
+  //   if (root == nullptr)
+  //     return {};
+  //
+  //   vector<int> answer;
+  //   stack<TreeNode *> stack({root});
+  //
+  //   while (!stack.empty()) {
+  //     TreeNode *front = stack.top();
+  //     stack.pop();
+  //
+  //     answer.push_back(front->val);
+  //     if (front->right != nullptr)
+  //       stack.push(front->right);
+  //     if (front->left != nullptr)
+  //       stack.push(front->left);
+  //   }
+  //
+  //   return answer;
+  // }
+private:
+  void traversal(vector<int> &answer, TreeNode *root) {
+    if (root == nullptr)
+      return;
+
+    answer.push_back(root->val);
+    traversal(answer, root->left);
+    traversal(answer, root->right);
   }
 };
