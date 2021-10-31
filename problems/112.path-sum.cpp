8c4053c871cd8ae3eb721da96afa4313c3c67d22
WzDTj <j.dantong@gmail.com>
2021-11-01 07:39:01 +0800

    optimized the solution of problem 112

--- problems/112.path-sum.cpp
+++ problems/112.path-sum.cpp
@@ -3,19 +3,13 @@
 class Solution {
 public:
   bool hasPathSum(TreeNode *root, int targetSum) {
-    return check(root, targetSum, 0) || check(root, targetSum, 0);
-  }
-
-private:
-  bool check(TreeNode *node, int target, int current) {
-    if (node == nullptr)
+    if (root == nullptr)
       return false;
 
-    int sum = current + node->val;
-
-    if (node->left == nullptr && node->right == nullptr)
-      return target == sum;
+    if (root->left == nullptr && root->right == nullptr)
+      return targetSum == root->val;
     else
-      return check(node->left, target, sum) || check(node->right, target, sum);
+      return hasPathSum(root->left, targetSum - root->val) ||
+             hasPathSum(root->right, targetSum - root->val);
   }
 };
