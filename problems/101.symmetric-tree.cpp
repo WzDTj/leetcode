211687225836a9a69186af2ef6f038f08ee3670a
WzDTj <j.dantong@gmail.com>
2021-11-03 23:45:45 +0800

    added a solution for problem 101

--- problems/101.symmetric-tree.cpp
+++ problems/101.symmetric-tree.cpp
@@ -1,10 +1,43 @@
 #include "../structures/tree_node.cpp"
+#include <stack>
+
+using namespace std;
 
 class Solution {
 public:
   // 递归
 
-  // bool isSymmetric(TreeNode *root) { return valid(root->left, root->right); }
+  bool isSymmetric(TreeNode *root) { return valid(root->left, root->right); }
+
+  // 迭代
+
+  // bool isSymmetric(TreeNode *root) {
+  //   stack<TreeNode *> stack;
+  //
+  //   stack.push(root->left);
+  //   stack.push(root->right);
+  //
+  //   while (!stack.empty()) {
+  //     TreeNode *right = stack.top();
+  //     stack.pop();
+  //     TreeNode *left = stack.top();
+  //     stack.pop();
+  //
+  //     if (left == nullptr && right == nullptr)
+  //       continue;
+  //
+  //     if ((left == nullptr || right == nullptr) || (left->val != right->val))
+  //       return false;
+  //
+  //     stack.push(left->left);
+  //     stack.push(right->right);
+  //
+  //     stack.push(left->right);
+  //     stack.push(right->left);
+  //   }
+  //
+  //   return true;
+  // }
 
 private:
   bool valid(TreeNode *left, TreeNode *right) {
