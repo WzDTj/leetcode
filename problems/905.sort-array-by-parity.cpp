de9d2007c0e6fa544c0cf663f11508eb9cdd05f2
WzDTj <j.dantong@gmail.com>
2021-11-06 16:05:06 +0800

    solved problem 905

---
+++ problems/905.sort-array-by-parity.cpp
@@ -0,0 +1,21 @@
+#include <cstddef>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> sortArrayByParity(vector<int> &nums) {
+    int n = nums.size();
+    int l = 0, r = n - 1;
+
+    while (l < r) {
+      if (nums[l] % 2)
+        swap(nums[l], nums[r--]);
+      else
+        ++l;
+    }
+
+    return nums;
+  }
+};
