7696a7276ffef146788c44aebb5aa22f8e275c24
WzDTj <j.dantong@gmail.com>
2021-11-20 09:00:41 +0800

    solved problem 540

---
+++ problems/540.single-element-in-a-sorted-array.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int singleNonDuplicate(vector<int> &nums) {
+    int n = nums.size();
+
+    for (int i = 0; i < n - 1; i += 2) {
+      if (nums[i] != nums[i + 1])
+        return nums[i];
+    }
+
+    return nums[n - 1];
+  }
+};
