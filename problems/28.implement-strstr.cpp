319d053455eba955f3ffd9128ec6a813f86cf00a
WzDTj <j.dantong@gmail.com>
2021-11-22 23:12:03 +0800

    added a solution for problem 28

--- problems/28.implement-strstr.cpp
+++ problems/28.implement-strstr.cpp
@@ -1,33 +1,71 @@
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
+  // KMP
+
   int strStr(string haystack, string needle) {
     int haystack_length = haystack.length();
     int needle_length = needle.length();
 
-    if (!needle_length)
-      return 0;
+    vector<int> next = generateNext(needle);
 
-    for (int i = 0; i < haystack_length; i++) {
-      if (haystack_length - i < needle_length)
-        break;
-
-      bool match = true;
-
-      for (int j = 0; j < needle_length; j++) {
-        if (haystack[i + j] != needle[j]) {
-          match = false;
-          break;
-        }
+    int i = 0, j = 0;
+    while (i < haystack_length && j < needle_length) {
+      if (j == -1 || haystack[i] == needle[j]) {
+        ++i, ++j;
+      } else {
+        j = next[j];
       }
-
-      if (match)
-        return i;
     }
 
-    return -1;
+    return needle_length <= j ? i - j : -1;
   }
+
+private:
+  vector<int> generateNext(string str) {
+    int n = str.length();
+
+    vector<int> next(n, -1);
+
+    int i = -1, j = 0;
+
+    while (j < n - 1) {
+      i == -1 || str[i] == str[j] ? next[++j] = ++i : i = next[i];
+    }
+
+    return next;
+  }
+
+  // 暴力
+
+  // int strStr(string haystack, string needle) {
+  //   int haystack_length = haystack.length();
+  //   int needle_length = needle.length();
+  //
+  //   if (!needle_length)
+  //     return 0;
+  //
+  //   for (int i = 0; i < haystack_length; i++) {
+  //     if (haystack_length - i < needle_length)
+  //       break;
+  //
+  //     bool match = true;
+  //
+  //     for (int j = 0; j < needle_length; j++) {
+  //       if (haystack[i + j] != needle[j]) {
+  //         match = false;
+  //         break;
+  //       }
+  //     }
+  //
+  //     if (match)
+  //       return i;
+  //   }
+  //
+  //   return -1;
+  // }
 };
