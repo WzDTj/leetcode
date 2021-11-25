c5f7d9c77c0e371e107480593f01a530a3e83f52
WzDTj <j.dantong@gmail.com>
2021-11-25 08:42:09 +0800

    solved problem 215

---
+++ problems/215.kth-largest-element-in-an-array.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int findKthLargest(vector<int> &nums, int k) {
+    sort(nums.begin(), nums.end(), greater<int>());
+
+    return nums[k - 1];
+  }
+};
