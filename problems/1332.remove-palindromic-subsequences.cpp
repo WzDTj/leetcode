d19a620e05293cd68db58a8cd6bb80feb39ce0b8
WzDTj <j.dantong@gmail.com>
2022-01-22 06:10:41 +0800

    solved problem 1332

---
+++ problems/1332.remove-palindromic-subsequences.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  int removePalindromeSub(string s) {
+    int n = s.length();
+
+    for (int i = 0; i < n / 2; ++i) {
+      if (s[i] != s[n - i - 1]) {
+        return 2;
+      }
+    }
+
+    return 1;
+  }
+};
