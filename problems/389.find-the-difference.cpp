104e8aa3ca7b3da850d39ad04dd35e3da08b0bb2
WzDTj <j.dantong@gmail.com>
2022-02-07 23:48:37 +0800

    solved problem 389

---
+++ problems/389.find-the-difference.cpp
@@ -0,0 +1,21 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  char findTheDifference(string s, string t) {
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+
+    int n = s.length();
+
+    for (int i = 0; i < n; ++i) {
+      if (s[i] != t[i]) {
+        return t[i];
+      }
+    }
+
+    return t[n];
+  }
+};
