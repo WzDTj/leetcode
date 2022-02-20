941d04b4e24b82813446424636e4126fe2d78bc6
WzDTj <j.dantong@gmail.com>
2022-02-20 12:11:24 +0800

    added an solution for problem 717

--- problems/717.1-bit-and-2-bit-characters.cpp
+++ problems/717.1-bit-and-2-bit-characters.cpp
@@ -4,14 +4,30 @@ using namespace std;
 
 class Solution {
 public:
+  // 逆序遍历
+
   bool isOneBitCharacter(vector<int> &bits) {
     int n = bits.size();
 
-    int i = 0;
-    while (i < n - 1) {
-      i += bits[i] == 0 ? 1 : 2;
+    int i = n - 2, count = 0;
+    while (0 <= i && bits[i]) {
+      ++count;
+      --i;
     }
 
-    return i == n - 1;
+    return (count + 1) % 2;
   }
+
+  // 正序遍历
+
+  // bool isOneBitCharacter(vector<int> &bits) {
+  //   int n = bits.size();
+  //
+  //   int i = 0;
+  //   while (i < n - 1) {
+  //     i += bits[i] == 0 ? 1 : 2;
+  //   }
+  //
+  //   return i == n - 1;
+  // }
 };
