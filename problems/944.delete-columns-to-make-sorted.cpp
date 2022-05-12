9a697273432dde87e96c8d0c9b16151288ff24ed
WzDTj <j.dantong@gmail.com>
2022-05-12 13:56:33 +0800

    solved problem 944

---
+++ problems/944.delete-columns-to-make-sorted.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int minDeletionSize(vector<string> &strs) {
+    int m = strs.size();
+    int n = strs[0].size();
+
+    int answer = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int j = 1; j < m; ++j) {
+        if (strs[j][i] < strs[j - 1][i]) {
+          ++answer;
+          break;
+        }
+      }
+    }
+
+    return answer;
+  }
+};
