18adde7c8eba71c38c015248e52bf47a3ca87bee
WzDTj <j.dantong@gmail.com>
2022-06-02 14:29:10 +0800

    solved problem 867

---
+++ problems/867.transpose-matrix.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> transpose(vector<vector<int>> &matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    vector<vector<int>> answer(n, vector<int>(m, 0));
+
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        answer[j][i] = matrix[i][j];
+      }
+    }
+
+    return answer;
+  }
+};
