426f910145c839c0e210e1277ca35fbb73f84bcb
WzDTj <j.dantong@gmail.com>
2023-02-19 06:40:31 +0800

    solved problem 976

---
+++ problems/976.largest-perimeter-triangle.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int largestPerimeter(vector<int> &nums) {
+    sort(nums.begin(), nums.end(), greater<int>());
+
+    int n = nums.size();
+
+    for (int i = 0; i < n - 2; ++i) {
+      if (nums[i] < nums[i + 1] + nums[i + 2]) {
+        return nums[i] + nums[i + 1] + nums[i + 2];
+      }
+    }
+
+    return 0;
+  }
+};
